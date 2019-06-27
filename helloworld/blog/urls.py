852c1be2adb0847ef08de370fd8023184de735ff
shelsoncool <shelson.sayonetech@gmail.com>
2019-06-27 16:10:43 +0530

    First

--- helloworld/blog/urls.py
+++ helloworld/blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns=[
-    path('', views.post, name="firstpost")
+    path('', views.post, name="firstpost"),
+    path('post/<int:pk>/', views.post_detail, name='list'),
 ]
